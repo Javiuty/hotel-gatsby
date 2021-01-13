c6faa5f8fff7a78934191d562b9b9efa471d27bd
Javiuty <javiiescuadra@gmail.com>
2021-01-13 13:06:52 +0100

    imagen query graphql, datoCMS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react"
+import ImagenHotel from "../components/imagenHotel"
 import Layout from "../components/layout"
 
 const IndexPage = () => (
   <Layout>
-    <h1>Index</h1>
+    <ImagenHotel />
   </Layout>
 )
 
